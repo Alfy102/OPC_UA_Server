6cc343844cd8376a2265f559bd15951104f8ba4c
Aliff_Syazwan <aliffsyazwansalim.gmail.com>
2021-09-21 13:50:28 +0800

    found out to reverse parent search

--- opc_ua_server/gsh_opc_platform (multi device)/history_test.py
+++ opc_ua_server/gsh_opc_platform (multi device)/history_test.py
@@ -4,19 +4,17 @@ import sys
 import logging
 from asyncua import Client, Node, ua
 from datetime import datetime
-logging.basicConfig(level=logging.INFO)
-_logger = logging.getLogger('asyncua')
+#logging.basicConfig(level=logging.INFO)
+#_logger = logging.getLogger('asyncua')
 
 
 async def main():
     url = 'opc.tcp://localhost:4840/gshopcua/server'
     # url = 'opc.tcp://commsvr.com:51234/UA/CAS_UA_Server'
     async with Client(url=url) as client:
-        data = 1
-        source_time = datetime.utcnow()
-        var = client.get_node(ua.NodeId(2048, 2))
-        data_value = ua.DataValue(ua.Variant(data, ua.VariantType.Int64),SourceTimestamp=source_time, ServerTimestamp=source_time)
-        await var.write_value(data_value)
+        var = client.get_node(ua.NodeId(2167, 2))
+        test = await var.get_parent()
+        print(await test.read_browse_name())
        
 
 if __name__ == '__main__':
